e72904755d83bda059523cc0cf2e43c3eafecef7
Andy Thorne <contrabandvr@gmail.com>
2015-05-04 16:45:28 +0100

    Added bundle assets into template

--- CoreBundle/DependencyInjection/Compiler/BundleCompilerPass.php
+++ CoreBundle/DependencyInjection/Compiler/BundleCompilerPass.php
@@ -6,7 +6,6 @@ use GravityCMS\Component\Bundle\GravityBundle;
 use GravityCMS\CoreBundle\DependencyInjection\Compiler\Exception\InvalidGravityBundleException;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\Yaml\Yaml;
 
 /**
@@ -36,7 +35,7 @@ class BundleCompilerPass implements CompilerPassInterface
             /** @var GravityBundle $bundle */
             $bundle = new $bundleClass();
 
-            if(!$bundle instanceof GravityBundle){
+            if (!$bundle instanceof GravityBundle) {
                 throw new InvalidGravityBundleException($bundle);
             }
 
@@ -81,26 +80,30 @@ class BundleCompilerPass implements CompilerPassInterface
             }
 
 
-            $folder         = $bundle->getPath() . '/Resources/assets/css';
+            $folder = $bundle->getPath() . '/Resources/assets/sass';
             if (is_dir($folder)) {
-                $cssRoot   = '/css';
+                $cssRoot  = '/css';
                 $dir      = new \RecursiveDirectoryIterator($folder);
                 $ite      = new \RecursiveIteratorIterator($dir);
                 $fileList = new \RegexIterator($ite, '/.+\.s?css/', \RegexIterator::GET_MATCH);
 
                 foreach ($fileList as $files) {
                     foreach ($files as $file) {
-                        $destination = str_replace($bundle->getPath() . '/Resources/assets/css/', '', $file);
-                        $assetId     = 'gravity_module_core_css_' . str_replace(
+                        // skip _*.scss files
+                        if(strpos($file, '_') === 0){
+                            continue;
+                        }
+                        $destination = str_replace($bundle->getPath() . '/Resources/assets/sass', 'css', $file);
+                        $assetId     = 'gravity_module_' . $bundle->getGravityBundleName() . '_' . str_replace(
                                 ['/', '-', '.scss', '.css'],
                                 ['_', '_', '', ''],
                                 $destination
                             );
                         $assetPath   = '/cms/' . $bundle->getGravityBundleName() . '/' . $destination;
-                        $assetPath = str_replace('.scss', '.css', $assetPath);
+                        $assetPath   = str_replace('.scss', '.css', $assetPath);
 
                         $assetMap[$assetNamespace . $destination] = $assetPath;
-                        $cssFiles[] = $file;
+                        $cssFiles[]                               = $file;
 
                         $assetManagerDefinition->addMethodCall(
                             'setFormula',
@@ -169,7 +172,7 @@ class BundleCompilerPass implements CompilerPassInterface
             }
 
             // TODO: Finish loading module menu
-            if(file_exists($menuConfigFile = $bundle->getPath() . '/Resources/config/menu.yml')){
+            if (file_exists($menuConfigFile = $bundle->getPath() . '/Resources/config/menu.yml')) {
                 $menuConfig = Yaml::parse(file_get_contents($menuConfigFile));
                 foreach ($menuConfig as $menuName => $menu) {
                     $menu += [
@@ -199,8 +202,6 @@ class BundleCompilerPass implements CompilerPassInterface
              * @see http://symfony.com/doc/current/components/dependency_injection/compilation.html#dumping-the-configuration-for-performance
              */
         }
-
-
 
         $assetManagerDefinition->addMethodCall(
             'setFormula',
